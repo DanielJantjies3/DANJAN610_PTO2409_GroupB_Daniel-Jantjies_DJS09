683cff11c8cfeab8ca3b15eccda70cfe1292e62c
DanielJantjies <162917612+DanielJantjies@users.noreply.github.com>
2025-06-22 13:35:45 +0200

    Array Types

--- src/index.ts
+++ src/index.ts
@@ -41,14 +41,15 @@ const you: {
     lastName: string;
     isReturning: boolean;
     age: number;
+    stayedAt: string[];
 } = {
    firstName: 'Bobby',
    lastName: 'Brown',
    isReturning: true,
-   age: 35
+   age: 35,
+   stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
-console.log(you.userName)
 
 function populateUser(isReturning : boolean, userName: string ) {
     if (isReturning){
