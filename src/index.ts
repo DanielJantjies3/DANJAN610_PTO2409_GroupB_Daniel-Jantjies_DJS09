0a4e9bf1c805891ddee5cf27ffc08367ee3bbfe5
DanielJantjies <162917612+DanielJantjies@users.noreply.github.com>
2025-06-22 14:09:08 +0200

    Adding Properties

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,105 @@
-// Number Types mini-challenge 10 10.2
-// Write a function that will only accept numbers and attend to
-// all TypeScript weakness flags.
-// : number
-const returningUserDisplay = document.querySelector('#returning-user')
-const userNameDisplay = document.querySelector('#user')
-const reviewTotalDisplay = document.querySelector("#reviews");
+import { showReviewTotal, populateUser } from './utils'
+let isOpen: boolean
 
-let isOpen : boolean
+// Reviews
+const reviews : { 
+    name: string; 
+    stars: number; 
+    loyaltyUser: boolean; 
+    date: string
+    }[] = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
 
-const reviews : {
-   name: string;
-   stars: number;
-   loyaltyUser: boolean;
-   date: string;
-}[]= [
-  {
-    name: "Sheia",
-    stars: 5,
-    loyaltyUser: true,
-    date: "01-04-2021",
-  },
-  {
-    name: "Andrzej",
-    stars: 3,
-    loyaltyUser: false,
-    date: "28-03-2021",
-  },
-  {
-    name: "Omar",
-    stars: 4,
-    loyaltyUser: true,
-    date: "27-03-2021",
-  },
-];
-
-function showReviewTotal (value : number, reviewer: string, isLoyalty: boolean) { 
-  const iconDisplay = isLoyalty ? '⭐' : ''
-  reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer +  ' ' + iconDisplay
-}
-
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
-
+// User
 const you: {
-    firstName : string;
+    firstName: string;
     lastName: string;
     isReturning: boolean;
     age: number;
-    stayedAt: string[];
+    stayedAt: string[]
 } = {
-   firstName: 'Bobby',
-   lastName: 'Brown',
-   isReturning: true,
-   age: 35,
-   stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+    firstName: 'Bobby',
+    lastName: 'Brown',
+    isReturning: true,
+    age: 35,
+    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
-
-function populateUser(isReturning : boolean, userName: string ) {
-    if (isReturning){
-        returningUserDisplay.innerHTML = 'back'
+//Properties
+const properties : {
+    image: string;
+    title: string;
+    price: number;
+    location: {
+        firstLine: string;
+        city: string;
+        code: number;
+        country: string;
+    };
+    contact: string;
+    isAvailable: boolean;
+}[] = [
+    {
+        image: '',
+        title: 'Colombian Shack',
+        price: 45,
+        location: {
+            firstLine: 'shack 37',
+            city: 'Bogota',
+            code: 45632,
+            country: 'Colombia'
+        },
+        contact: 'marywinkle@gmail.com',
+        isAvailable: true  
+    },
+    {
+        image: '',
+        title: 'Polish Cottage',
+        price: 34,
+        location: {
+            firstLine: 'no 23',
+            city: 'Gdansk',
+            code: 343903,
+            country: 'Poland'
+        },
+        contact: 'garydavis@hotmail.com',
+        isAvailable: false 
     }
-    userNameDisplay.innerHTML = userName
-}
+        {
+        image: '',
+        title: 'London Flat',
+        price: 23,
+        location: {
+            firstLine: 'flat 15',
+            city: 'London',
+            code: 35433,
+            country: 'United Kingdom'
+        },
+        contact: 'andyluger@aol.com',
+        isAvailable: true
+    }
+]
 
-populateUser(you.isReturning, you.userName)
+// Functions
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+populateUser(you.isReturning, you.firstName)
+
